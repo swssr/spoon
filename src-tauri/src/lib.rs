ee6a2fd1f51b0ddc7e697cda06b2b6157d853bb2
Simo Ngquseka <simo@swssr.co>
2025-08-27 10:37:51 +0200

    Not sure

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,20 +1,31 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    match name.parse::<i64>() {
+        Ok(n) => {
+            format!(
+                "Hello, {}! You've been greeted from Rust! You're grown {bro}",
+                name,
+                bro = 20
+            )
+        }
+        Err(e) => format!("Bathong!"),
+    }
 }
 
 #[tauri::command]
-fn open_file(filePath: &str) {
-    println!("Opening {} on path", filePath);
-    format!("Yello, {}!", filePath);
+fn file(file_path: &str) {
+    //println!("Opening {} on path", file_path);
+    let path_name = "/Documents";
+    format!("Hello, {}! Path: {path_name}", file_path);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_cli::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, open_file])
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,39 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core";
+  // import * as monaco from "";
 
   let name = $state("");
-  let greetMsg = $state("");
+  let message = $state("");
+  let value = $state("");
+  let errorMessage = $state("");
 
   async function greet(event: Event) {
     event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
+    message = await invoke("greet", { name });
   }
+
+  async function listen(event: Event) {
+    try {
+      const name = event.target.value; 
+      debugger
+
+      message = await invoke("greet", { name });
+
+    } catch (e) {
+        errorMessage = e.message; 
+    }
+  }
+
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri + Svelte</h1>
-
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://kit.svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
-  </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
-
   <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name} oninput={listen} />
     <button type="submit">Greet</button>
   </form>
-  <p>{greetMsg}</p>
+
+  <p>{message}</p>
+
 </main>
 
 <style>
